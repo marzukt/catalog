22ae2b8cb71857f75c587615d908eeb018625519
marzukt <marzukt@gmail.com>
2015-10-15 12:21:12 +0100

    Change app port to 8000

--- application.py
+++ application.py
@@ -484,5 +484,5 @@ def showTest():
 if __name__ == '__main__':
     app.secret_key = 'change_me_to_something_secure'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8000)
 
